d2a208bcb8d53b9d07081f6e2d212e2a238c7b71
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2017-11-24 23:41:23 +0000

    Update sample data

--- sample-data/photography_SHUTTER_TYPE_data.sql
+++ sample-data/photography_SHUTTER_TYPE_data.sql
@@ -1,1 +1,1 @@
-INSERT INTO `SHUTTER_TYPE` VALUES (1,'Focal plane (horizontal travelling)'),(2,'Leaf'),(3,'Rotary'),(4,'Focal plane (vertical travelling)'),(5,'Sliding');
+INSERT INTO `SHUTTER_TYPE` VALUES (1,'Focal plane (horizontal travelling)'),(2,'Leaf'),(3,'Rotary'),(4,'Focal plane (vertical travelling)'),(5,'Sliding'),(6,'Ball bearing'),(7,'Electronic');
