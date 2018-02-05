bee0aad01088f852de13e702a4ecd952730b95fa
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2018-02-05 21:38:22 +0000

    Dump functions for the first time

---
+++ schema/photography_functions.sql
@@ -0,0 +1,24 @@
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`jonathan`@`%` FUNCTION `lookupneg`(p_film_id int, p_frame varchar(2)) RETURNS int(11)
+    DETERMINISTIC
+BEGIN
+	declare negid int;
+    select negative_id into negid
+    from NEGATIVE
+    where film_id = p_film_id
+		and frame = p_frame;
+RETURN negid;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
