d042ddea0bfdacd39351f06a9b2702970d9d7839
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2018-06-25 12:43:55 +0100

    Switch export method to avoid having to implicitly import

--- lib/funcs.pm
+++ lib/funcs.pm
@@ -7,7 +7,7 @@ use Exporter qw(import);
 use Data::Dumper;
 use Config::IniHash;
 
-our @EXPORT_OK = qw(prompt db updaterecord newrecord notimplemented nocommand nosubcommand help listchoices lookupval updatedata today validate);
+our @EXPORT = qw(prompt db updaterecord newrecord notimplemented nocommand nosubcommand help listchoices lookupval updatedata today validate);
 
 # Prompt for an arbitrary value
 sub prompt {
