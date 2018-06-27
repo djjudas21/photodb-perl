4e556ede26e01f620492fa2b20e1a0c1958a9a97
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2018-06-27 09:48:00 +0100

    Move more functions over to handlers

--- lib/handlers.pm
+++ lib/handlers.pm
@@ -13,7 +13,7 @@ use lib 'lib';
 use funcs;
 use queries;
 
-our @EXPORT = qw(film_add);
+our @EXPORT = qw(film_add film_load film_develop);
 
 sub film_add {
 	# Add a newly-purchased film
@@ -32,4 +32,34 @@ sub film_add {
 	$data{'purchase_date'} = prompt(&today($db), 'Purchase date', 'date');
 	$data{'price'} = prompt('', 'Purchase price', 'decimal');
 	&newrecord($db, \%data, 'FILM');
+}
+
+sub film_load {
+	# Load a film into a camera
+	my $db = shift;
+	my %data;
+	my $film_id = &listchoices($db, 'film', "select * from choose_film_to_load");
+	$data{'exposed_at'} = prompt(&lookupval($db, "select iso from FILM, FILMSTOCK where FILM.filmstock_id=FILMSTOCK.filmstock_id and film_id=$film_id"), 'What ISO?', 'integer');
+	$data{'date_loaded'} = prompt(&today($db), 'What date was this film loaded?', 'date');
+	$data{'camera_id'} = &listchoices($db, 'camera', "select C.camera_id as id, concat(M.manufacturer, ' ', C.model) as opt from CAMERA as C, FILM as F, MANUFACTURER as M where F.format_id=C.format_id and C.manufacturer_id=M.manufacturer_id and film_id=$film_id and own=1");
+	$data{'notes'} = prompt('', 'Notes', 'text');
+	&updaterecord($db, \%data, 'FILM', "film_id=$film_id");
+}
+
+sub film_develop {
+	# Develop a film
+	my $db = shift;
+	my %data;
+	my $film_id = &listchoices($db, 'film', "select * from choose_film_to_develop");
+	$data{'date'} = prompt(&today($db), 'What date was this film processed?', 'date');
+	$data{'developer_id'} = &listchoices($db, 'developer', "select developer_id as id, name as opt from DEVELOPER where for_film=1");
+	$data{'directory'} = prompt('', 'What directory are these scans in?', 'text');
+	$data{'photographer_id'} = &listchoices($db, 'photographer', "select photographer_id as id, name as opt from PHOTOGRAPHER");
+	$data{'dev_uses'} = prompt('', 'How many previous uses has the developer had?', 'integer');
+	$data{'dev_time'} = prompt('', 'How long was the film developed for?', 'hh:mm:ss');
+	$data{'dev_temp'} = prompt('', 'What temperature was the developer?', 'decimal');
+	$data{'dev_n'} = prompt(0, 'What push/pull was used?', 'integer');
+	$data{'development_notes'} = prompt('', 'Any other development notes', 'text');
+	$data{'processed_by'} = prompt('', 'Who developed the film?', 'text');
+	&updaterecord($db, \%data, 'FILM', "film_id=$film_id");
 }
