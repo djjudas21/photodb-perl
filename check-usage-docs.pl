cf0238ac02e4017f26df34db0cd485fecf868e17
Jonathan Gazeley <me@jonathangazeley.com>
2018-08-17 16:19:04 +0100

    Add new Travis test for documentation completeness

---
+++ check-usage-docs.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use lib 'lib';
+use commands;
+
+# Default to a clean return code
+my $return = 0;
+
+# Filename of USAGE doc
+my $filename = 'docs/USAGE.md';
+
+# Load command mapping
+my %handlers = %commands::handlers;
+
+for my $command (sort keys %handlers) {
+	# search for command heading
+	# ## `command`
+	if (system(qq{grep -q "^## \\`$command\\`" $filename})) {
+		$return = 1;
+		print "Could not find documentation for command \"$command\"\n";
+	}
+	for my $subcommand (sort keys %{$handlers{$command}}) {
+		# search for subcommand heading
+		# ### `command subcommand`
+		if (system(qq{grep -q "^### \\`$command $subcommand\\`" $filename})) {
+			$return = 1;
+			print "Could not find documentation for subcommand \"$command $subcommand\"\n";
+		}
+	}
+}
+
+if ($return == 0) {
+	print "Documentation seems to contain a section for every command and subcommand\n";
+}
+exit $return;
