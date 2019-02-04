7585720f3e63fbfaefc9bdbafdf93d0271553f7e
Jonathan Gazeley <me@jonathangazeley.com>
2019-02-04 13:15:28 +0000

    Autoincrement PK

--- schema/photography_SCAN.sql
+++ schema/photography_SCAN.sql
@@ -1,7 +1,7 @@
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `SCAN` (
-  `scan_id` int(11) NOT NULL COMMENT 'Unique ID for this scan',
+  `scan_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique ID for this scan',
   `negative_id` int(11) DEFAULT NULL COMMENT 'ID of the negative that was scanned',
   `print_id` int(11) DEFAULT NULL COMMENT 'ID of the print  that was scanned',
   `filename` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '''Filename of the scan''',
