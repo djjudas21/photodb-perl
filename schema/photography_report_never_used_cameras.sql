76c2e7c3a8392e5e2e135e62ecf160f157150591
Jonathan Gazeley <me@jonathangazeley.com>
2019-02-13 16:29:12 +0000

    Rename report views and add them to the handler

--- schema/photography_report_never_used_cameras.sql
+++ schema/photography_report_never_used_cameras.sql
@@ -1,12 +1,12 @@
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
-/*!50001 CREATE TABLE `never_used_lenses` (
-  `lens_id` tinyint NOT NULL,
+/*!50001 CREATE TABLE `report_never_used_cameras` (
+  `camera_id` tinyint NOT NULL,
   `manufacturer` tinyint NOT NULL,
   `model` tinyint NOT NULL
 ) ENGINE=MyISAM */;
 SET character_set_client = @saved_cs_client;
-/*!50001 DROP TABLE IF EXISTS `never_used_lenses`*/;
+/*!50001 DROP TABLE IF EXISTS `report_never_used_cameras`*/;
 /*!50001 SET @saved_cs_client          = @@character_set_client */;
 /*!50001 SET @saved_cs_results         = @@character_set_results */;
 /*!50001 SET @saved_col_connection     = @@collation_connection */;
@@ -15,7 +15,7 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = utf8_general_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`jonathan`@`%` SQL SECURITY DEFINER */
-/*!50001 VIEW `never_used_lenses` AS select `LENS`.`lens_id` AS `lens_id`,`MANUFACTURER`.`manufacturer` AS `manufacturer`,`LENS`.`model` AS `model` from ((`MANUFACTURER` join `MOUNT`) join (`LENS` left join `NEGATIVE` on((`NEGATIVE`.`lens_id` = `LENS`.`lens_id`)))) where (isnull(`NEGATIVE`.`lens_id`) and (`LENS`.`fixed_mount` = 0) and (`MOUNT`.`purpose` = 'Camera') and (`LENS`.`mount_id` = `MOUNT`.`mount_id`) and (`MOUNT`.`digital_only` = 0) and (`LENS`.`manufacturer_id` = `MANUFACTURER`.`manufacturer_id`) and (`LENS`.`own` = 1)) order by `LENS`.`lens_id` */;
+/*!50001 VIEW `report_never_used_cameras` AS select `CAMERA`.`camera_id` AS `camera_id`,`MANUFACTURER`.`manufacturer` AS `manufacturer`,`CAMERA`.`model` AS `model` from ((`CAMERA` left join `FILM` on((`CAMERA`.`camera_id` = `FILM`.`camera_id`))) left join `MANUFACTURER` on((`CAMERA`.`manufacturer_id` = `MANUFACTURER`.`manufacturer_id`))) where (isnull(`FILM`.`camera_id`) and (`CAMERA`.`own` <> 0) and (`CAMERA`.`digital` = 0) and (`CAMERA`.`video` = 0)) */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
--- schema/photography_report_never_used_lenses.sql
+++ schema/photography_report_never_used_lenses.sql
@@ -1,12 +1,12 @@
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
-/*!50001 CREATE TABLE `total_negatives_per_lens` (
+/*!50001 CREATE TABLE `report_never_used_lenses` (
+  `lens_id` tinyint NOT NULL,
   `manufacturer` tinyint NOT NULL,
-  `model` tinyint NOT NULL,
-  `count(negative_id)` tinyint NOT NULL
+  `model` tinyint NOT NULL
 ) ENGINE=MyISAM */;
 SET character_set_client = @saved_cs_client;
-/*!50001 DROP TABLE IF EXISTS `total_negatives_per_lens`*/;
+/*!50001 DROP TABLE IF EXISTS `report_never_used_lenses`*/;
 /*!50001 SET @saved_cs_client          = @@character_set_client */;
 /*!50001 SET @saved_cs_results         = @@character_set_results */;
 /*!50001 SET @saved_col_connection     = @@collation_connection */;
@@ -15,7 +15,7 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = utf8_general_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`jonathan`@`%` SQL SECURITY DEFINER */
-/*!50001 VIEW `total_negatives_per_lens` AS select `MANUFACTURER`.`manufacturer` AS `manufacturer`,`LENS`.`model` AS `model`,count(`NEGATIVE`.`negative_id`) AS `count(negative_id)` from ((`LENS` join `NEGATIVE`) join `MANUFACTURER`) where ((`LENS`.`lens_id` = `NEGATIVE`.`lens_id`) and (`LENS`.`manufacturer_id` = `MANUFACTURER`.`manufacturer_id`)) group by `LENS`.`lens_id` order by count(`NEGATIVE`.`negative_id`) desc */;
+/*!50001 VIEW `report_never_used_lenses` AS select `LENS`.`lens_id` AS `lens_id`,`MANUFACTURER`.`manufacturer` AS `manufacturer`,`LENS`.`model` AS `model` from ((`MANUFACTURER` join `MOUNT`) join (`LENS` left join `NEGATIVE` on((`NEGATIVE`.`lens_id` = `LENS`.`lens_id`)))) where (isnull(`NEGATIVE`.`lens_id`) and (`LENS`.`fixed_mount` = 0) and (`MOUNT`.`purpose` = 'Camera') and (`LENS`.`mount_id` = `MOUNT`.`mount_id`) and (`MOUNT`.`digital_only` = 0) and (`LENS`.`manufacturer_id` = `MANUFACTURER`.`manufacturer_id`) and (`LENS`.`own` = 1)) order by `LENS`.`lens_id` */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
