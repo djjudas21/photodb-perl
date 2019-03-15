a1b53b5124c363402715ab829bb64d94f0d5578e
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2019-03-15 14:31:16 +0000

    Merge INSTALL.md into pod

--- lib/App/PhotoDB.pm
+++ lib/App/PhotoDB.pm
@@ -172,6 +172,82 @@ All of the above "special" accessories can be added to the database with C<photo
 Types of general accessory with no special properties could include cases or straps. General accessories can be associated with cameras or lenses, or neither.
 Add new general accessories with C<photodb accessory add> and add new categories of general accessories with C<photodb accessory category>.
 
+=head1 Installing PhotoDB
+
+=head2 Install database backend
+
+A pre-requisite for PhotoDB is a functioning MySQL or MariaDB database instance. If you have access to an existing
+MySQL server, e.g. at a hosting provider, note down the details for connecting (hostname or IP address, username, password).
+
+Otherwise, consider installing a MySQL server locally on your computer. If you do this, the hostname will be C<localhost>.
+
+Create a database user with a password for PhotoDB with sufficient privileges to SELECT, INSERT, UPDATE, DELETE, CREATE and DROP.
+
+=head2 Install application frontend
+
+There are several different ways to install the application. Choose your favourite.
+Install PhotoDB from CPAN or from a source tarball.
+
+=head3 From CPAN
+
+1. Use the C<cpan> client (or an alternative, such as C<cpanm>) to install directly:
+
+    cpan App::PhotoDB
+
+or
+
+    cpanm App::PhotoDB
+
+=head3 From source
+
+1. Grab the latest release tarball from the [Releases page](https://github.com/djjudas21/photography-database/releases).
+It's the one with a name like C<App-PhotoDB-0.00.tar.gz>.
+
+2. Install it with C<cpanm>:
+
+    cpanm /path/tp/App-PhotoDB-0.00.tar.gz
+
+=head3 Docker
+
+Run PhotoDB on any platform, with Docker
+
+1. Fetch the Docker image
+
+    docker pull djjudas21/photodb
+
+2. Run the image
+
+    docker run -it --rm -v ~/.photodb:/root/.photodb --name photodb photodb
+
+=head3 Set up autocomplete
+
+PhotoDB ships with Bash completion. Enable it by
+
+    echo "source photodb-completion.bash" >> ~/.bash_profile
+
+=head2 Configure database connection
+
+There are three methods for connecting to the database:
+1. Database and application on same computer
+2. Database and application on different computers, connect via native MySQL
+3. Database and application on different computers, connect via SSH tunnel
+
+The app and accessory scripts need to know how to connect to the database. The first time you run
+PhotoDB, you will be prompted to enter connection details for the database backend. If you need to
+edit the config in future, the config file is created at C</etc/photodb.ini>.
+
+=head3 Tunnelling
+
+If the database is on a remote server and does not have the MySQL port (3306) open to receive connections,
+you will need to set up a tunnel. Run the command below, substituting in the correct hostname and
+username for the database server.
+
+    ssh -L 3306:localhost:3306 -N <username>@<database.example.com>
+
+Once the tunnel is established, you should be able to connect to the database on C<127.0.0.1:3306> and
+your connection will be tunnelled. Configure PhotoDB in the same way as if the database was local.
+You will need to re-establish the tunnel each time you wish to use PhotoDB.
+
 =head1 BUGS/CAVEATS/etc
 
 Report bugs in the L<Github issue tracker|https://github.com/djjudas21/photography-database/issues>
@@ -183,8 +259,6 @@ Jonathan Gazeley
 =head1 SEE ALSO
 
 =over
-
-=item * L<Installing PhotoDB|docs/INSTALL.md>
 
 =item * L<Developing PhotoDB|docs/CONTRIBUTING.md>
 
