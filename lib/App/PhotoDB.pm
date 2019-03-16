ded55e9ed12b25150e32c6f2481d689be9a0a222
Jonathan Gazeley <me@jonathangazeley.com>
2019-03-16 22:24:46 +0000

    Drop non-interactive mode

--- lib/App/PhotoDB.pm
+++ lib/App/PhotoDB.pm
@@ -219,12 +219,6 @@ Run PhotoDB on any platform, with Docker
 
     docker run -it --rm -v ~/.photodb:/root/.photodb --name photodb photodb
 
-=head3 Set up autocomplete
-
-PhotoDB ships with Bash completion. Enable it by
-
-    echo "source photodb-completion.bash" >> ~/.bash_profile
-
 =head2 Configure database connection
 
 There are three methods for connecting to the database:
@@ -287,71 +281,41 @@ sub main {
 	# Define handlers for each command
 	my %handlers = %App::PhotoDB::commands::handlers;
 
-	# Check if any args were passed in
-	if (defined($ARGV[0])) {
-		# We got at least one cmdline arg, so we run in one-shot mode
+	&welcome;
 
-		# Read in and verify command
-		my $command = $ARGV[0];
-		if ((!defined $command) || (!exists $handlers{$command})) {
-			&nocommand(\%handlers);
+	# Connect to the database
+	my $db = &db;
+
+	# Enter interactive prompt and loop until exited by user
+	while (1) {
+		my $rv = &prompt({prompt=>'photodb', type=>'text', showtype=>0, showdefault=>0, char=>'>'});
+		# Trap important keywords first
+		if ($rv eq 'exit' || $rv eq 'quit') {
 			exit;
-		}
+		} elsif ($rv =~ /^(\w+) ?([\w-]+)?$/) {
+			# Match a command and an optional subcommand
+			# Check if the command is defined in the handlers
+			my $command = $1;
+			if (!exists $handlers{$command}) {
+				&nocommand(\%handlers);
+				next;
+			}
 
-		# Read in and verify subcommand
-		my $subcommand = $ARGV[1];
-		if ((!defined $subcommand) || (!exists $handlers{$command}{$subcommand})) {
-			&nosubcommand(\%{$handlers{$command}}, $command);
-			exit;
-		}
+			# Check if the subcommand is defined in the handlers
+			my $subcommand = $2;
+			if (!exists $handlers{$command}{$subcommand}) {
+				&nosubcommand(\%{$handlers{$command}}, $command);
+				next;
+			}
 
-		# Connect to the database
-		my $db = &db;
-
-		# Execute chosen handler
-		if (&prompt({prompt=>"$handlers{$command}{$subcommand}{'desc'}. Continue?", type=>'boolean', default=>'yes'})) {
-			$handlers{$command}{$subcommand}{'handler'}->($db);
-		}
-
-		# Exit after one job
-		exit;
-	} else {
-		# No cmdline args, so enter interactive prompt and loop until exited by user
-		&welcome;
-
-		# Connect to the database
-		my $db = &db;
-
-		while (1) {
-			my $rv = &prompt({prompt=>'photodb', type=>'text', showtype=>0, showdefault=>0, char=>'>'});
-			# Trap important keywords first
-			if ($rv eq 'exit' || $rv eq 'quit') {
-				exit;
-			} elsif ($rv =~ /^(\w+) ?([\w-]+)?$/) {
-				# Match a command and an optional subcommand
-				# Check if the command is defined in the handlers
-				my $command = $1;
-				if (!exists $handlers{$command}) {
-					&nocommand(\%handlers);
-					next;
-				}
-
-				# Check if the subcommand is defined in the handlers
-				my $subcommand = $2;
-				if (!exists $handlers{$command}{$subcommand}) {
-					&nosubcommand(\%{$handlers{$command}}, $command);
-					next;
-				}
-
-				# Execute chosen handler
-				if (&prompt({prompt=>"$handlers{$command}{$subcommand}{'desc'}. Continue?", type=>'boolean', default=>'yes'})) {
-					$handlers{$command}{$subcommand}{'handler'}->($db);
-				}
-			} else {
-				# Print list of commands if unknown input is entered
-				if ($rv ne '') {
-					&nocommand(\%handlers);
-				}
+			# Execute chosen handler
+			if (&prompt({prompt=>"$handlers{$command}{$subcommand}{'desc'}. Continue?", type=>'boolean', default=>'yes'})) {
+				$handlers{$command}{$subcommand}{'handler'}->($db);
+			}
+		} else {
+			# Print list of commands if unknown input is entered
+			if ($rv ne '') {
+				&nocommand(\%handlers);
 			}
 		}
 	}
