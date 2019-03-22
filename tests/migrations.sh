145df835d4dd42e517b5597931b18c032a52e513
Jonathan Gazeley <me@jonathangazeley.com>
2019-03-22 19:05:05 +0000

    Use expect to run interactive tests, can't just use printf with Readline

--- tests/migrations.sh
+++ tests/migrations.sh
@@ -6,7 +6,7 @@ perl Build.PL
 ./Build install
 
 # Install DB
-sudo apt-get update && sudo apt-get install mysql-server-5.6
+sudo apt-get update && sudo apt-get install -y mysql-server-5.6
 mysql -uroot -e "create schema photodb; GRANT ALL PRIVILEGES ON photodb.* TO photodb@localhost IDENTIFIED BY 'photodb'"
 
 # Configure PhotoDB
@@ -21,4 +21,5 @@ EOT
 
 # Run Photodb to trigger migrations
 # Exit straight away, but this still runs migrations
-printf 'exit' | photodb
+sudo apt-get install -y expect
+./tests/migrations.exp
