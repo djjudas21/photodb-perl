b7a9aac6f87df37dc5745fefa30bcee80a0139ba
Jonathan Gazeley <me@jonathangazeley.com>
2019-04-04 15:47:05 +0100

    Schema changes that should've been captured in a previous commit

--- schema/photography_SHUTTER_SPEED.sql
+++ schema/photography_SHUTTER_SPEED.sql
@@ -2,7 +2,7 @@
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `SHUTTER_SPEED` (
   `shutter_speed` varchar(10) CHARACTER SET latin1 NOT NULL COMMENT 'Shutter speed in fractional notation, e.g. 1/250',
-  `duration` decimal(7,5) DEFAULT NULL COMMENT 'Shutter speed in decimal notation, e.g. 0.04',
+  `duration` decimal(9,5) DEFAULT NULL COMMENT 'Shutter speed in decimal notation, e.g. 0.04',
   PRIMARY KEY (`shutter_speed`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Table to list all possible shutter speeds';
 /*!40101 SET character_set_client = @saved_cs_client */;
