dd42f56673e51d51b1d5c0d284a1b1500ce6e290
Jonathan Gazeley <me@jonathangazeley.com>
2019-04-11 22:55:28 +0100

    Add stored procedures for all tasks

--- schema/photography_functions.sql
+++ schema/photography_functions.sql
@@ -84,6 +84,23 @@ END ;;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+CREATE PROCEDURE `delete_logs`()
+BEGIN
+DELETE from LOG
+WHERE datetime < date_sub(now(), interval 90 day);
+END ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
 /*!50003 SET character_set_client  = utf8 */ ;
 /*!50003 SET character_set_results = utf8 */ ;
 /*!50003 SET collation_connection  = utf8_general_ci */ ;
@@ -92,6 +109,232 @@ END ;;
 CREATE PROCEDURE `print_unarchive`(IN printId int)
 BEGIN
 UPDATE PRINT set archive_id = null WHERE print_id = printId;
+END ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+CREATE PROCEDURE `set_fixed_lenses`()
+BEGIN
+UPDATE
+    LENS,
+    CAMERA
+SET
+    LENS.own=0,
+    LENS.lost=CAMERA.lost
+WHERE
+    LENS.lens_id=CAMERA.lens_id
+        and CAMERA.own=0
+        and CAMERA.fixed_mount=1;
+END ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+CREATE PROCEDURE `update_dates_of_fixed_lengths`()
+BEGIN
+UPDATE
+    LENS,
+    CAMERA
+SET
+    LENS.acquired=CAMERA.acquired
+WHERE
+    LENS.lens_id = CAMERA.lens_id
+        and CAMERA.fixed_mount = 1
+        and CAMERA.acquired is not null
+        and LENS.acquired!=CAMERA.acquired;
+END ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+CREATE PROCEDURE `update_exposure_programs`()
+BEGIN
+UPDATE
+    NEGATIVE,
+    FILM,
+    CAMERA,
+    EXPOSURE_PROGRAM_AVAILABLE,
+    (SELECT
+        CAMERA.camera_id
+    FROM
+        CAMERA, EXPOSURE_PROGRAM_AVAILABLE
+    where
+        CAMERA.camera_id = EXPOSURE_PROGRAM_AVAILABLE.camera_id
+    group by camera_id
+    having count(exposure_program_id) = 1
+    ) as VALIDCAMERA
+    set
+        NEGATIVE.exposure_program = EXPOSURE_PROGRAM_AVAILABLE.exposure_program_id
+    where
+        CAMERA.camera_id = EXPOSURE_PROGRAM_AVAILABLE.camera_id
+            and EXPOSURE_PROGRAM_AVAILABLE.exposure_program_id <> 0
+            and NEGATIVE.film_id=FILM.film_id
+            and FILM.camera_id=CAMERA.camera_id
+            and CAMERA.camera_id = VALIDCAMERA.camera_id
+            and NEGATIVE.exposure_program is null;
+END ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+CREATE PROCEDURE `update_focal_length`()
+BEGIN
+UPDATE
+    NEGATIVE left join TELECONVERTER on (NEGATIVE.teleconverter_id=TELECONVERTER.teleconverter_id),
+    LENS
+SET
+    NEGATIVE.focal_length=round(LENS.min_focal_length * coalesce(TELECONVERTER.factor,1))
+WHERE
+   NEGATIVE.lens_id=LENS.lens_id
+   and LENS.zoom = 0
+   and LENS.min_focal_length is not null
+   and NEGATIVE.focal_length is null;
+END ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+CREATE PROCEDURE `update_lens_id_fixed_camera`()
+BEGIN
+UPDATE NEGATIVE,
+    LENS,
+    CAMERA,
+    FILM 
+SET 
+    NEGATIVE.lens_id = LENS.lens_id
+WHERE
+    NEGATIVE.film_id = FILM.film_id
+        AND FILM.camera_id = CAMERA.camera_id
+        AND CAMERA.fixed_mount = 1
+        AND CAMERA.lens_id = LENS.lens_id;
+END ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+CREATE PROCEDURE `update_metering_modes`()
+BEGIN
+UPDATE
+    NEGATIVE,
+    FILM,
+    CAMERA,
+    METERING_MODE_AVAILABLE,
+    (SELECT
+        CAMERA.camera_id
+    FROM
+        CAMERA, METERING_MODE_AVAILABLE
+    where
+        CAMERA.camera_id = METERING_MODE_AVAILABLE.camera_id
+    group by camera_id
+    having count(METERING_MODE_AVAILABLE.metering_mode_id) = 1
+    ) as VALIDCAMERA
+SET
+    NEGATIVE.metering_mode = METERING_MODE_AVAILABLE.metering_mode_id
+WHERE
+    CAMERA.camera_id = METERING_MODE_AVAILABLE.camera_id
+        and METERING_MODE_AVAILABLE.metering_mode_id <> 0
+        and NEGATIVE.film_id=FILM.film_id
+        and FILM.camera_id=CAMERA.camera_id
+        and CAMERA.camera_id = VALIDCAMERA.camera_id
+        and NEGATIVE.metering_mode is null;
+END ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+CREATE PROCEDURE `update_negative_flash`()
+BEGIN
+UPDATE
+    CAMERA
+    join FILM on FILM.camera_id=CAMERA.camera_id
+    join NEGATIVE on NEGATIVE.film_id = FILM.film_id
+SET
+    NEGATIVE.flash = 0
+WHERE
+    int_flash=0
+    and ext_flash=0
+    and NEGATIVE.flash is null;
+END ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+CREATE PROCEDURE `update_negative_sizes`()
+BEGIN
+UPDATE
+    NEGATIVE_SIZE
+SET
+    crop_factor = round(sqrt(24*24 + 36*36)/sqrt(width*width + height*height),2),
+    area = width*height,
+    aspect_ratio = round(width/height, 2)
+WHERE
+    width is not null
+        and height is not null;
 END ;;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
