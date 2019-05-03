7a69b192b5a1c8a675647cca4e8105b1ca97a988
Jonathan Gazeley <me@jonathangazeley.com>
2019-05-03 14:13:46 +0100

    Install expect via Travis builtins

--- tests/migrations.sh
+++ tests/migrations.sh
@@ -17,7 +17,6 @@ EOT
 
 # Run Photodb to trigger migrations
 # Exit straight away, but this still runs migrations
-sudo apt-get install -y expect
 ./tests/migrations.exp
 
 # Test all views by selecting from them
