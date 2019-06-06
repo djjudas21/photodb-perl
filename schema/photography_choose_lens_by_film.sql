72dbcb37d1815171d27ecc29d02e325d3a044ee0
Jonathan Gazeley <me@jonathangazeley.com>
2019-06-06 16:51:03 +0100

    Update views to refer to cameras and lenses with their serial number

--- schema/photography_choose_lens_by_film.sql
+++ schema/photography_choose_lens_by_film.sql
@@ -15,7 +15,7 @@ SET character_set_client = @saved_cs_client;
 /*!50001 SET collation_connection      = utf8mb4_general_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`photography`@`%` SQL SECURITY DEFINER */
-/*!50001 VIEW `choose_lens_by_film` AS select `LENS`.`lens_id` AS `id`,`LENSMODEL`.`model` AS `opt`,`FILM`.`film_id` AS `film_id` from ((((`FILM` join `CAMERA` on(`FILM`.`camera_id` = `CAMERA`.`camera_id`)) join `CAMERAMODEL` on(`CAMERA`.`cameramodel_id` = `CAMERAMODEL`.`cameramodel_id`)) join `LENSMODEL` on(`CAMERAMODEL`.`mount_id` = `LENSMODEL`.`mount_id`)) join `LENS` on(`LENS`.`lensmodel_id` = `LENS`.`lensmodel_id`)) */;
+/*!50001 VIEW `choose_lens_by_film` AS select `LENS`.`lens_id` AS `id`,concat(`MANUFACTURER`.`manufacturer`,' ',`LENSMODEL`.`model`,if(`LENS`.`serial`,concat(' (#',`LENS`.`serial`,')'),'')) AS `opt`,`FILM`.`film_id` AS `film_id` from (((((`FILM` join `CAMERA` on(`FILM`.`camera_id` = `CAMERA`.`camera_id`)) join `CAMERAMODEL` on(`CAMERA`.`cameramodel_id` = `CAMERAMODEL`.`cameramodel_id`)) join `LENSMODEL` on(`CAMERAMODEL`.`mount_id` = `LENSMODEL`.`mount_id`)) join `MANUFACTURER` on(`LENSMODEL`.`manufacturer_id` = `MANUFACTURER`.`manufacturer_id`)) join `LENS` on(`LENSMODEL`.`lensmodel_id` = `LENS`.`lensmodel_id`)) */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
