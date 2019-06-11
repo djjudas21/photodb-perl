36b3b59fe9ae313ab317a06a218e3589c8513748
Jonathan Gazeley <me@jonathangazeley.com>
2019-06-11 16:44:59 +0100

    Take out references to schema

--- migrations/042-negative-format.sql
+++ migrations/042-negative-format.sql
@@ -1,16 +1,16 @@
-CREATE TABLE `photography`.`NEGATIVEFORMAT_COMPAT` (
+CREATE TABLE `NEGATIVEFORMAT_COMPAT` (
   `format_id` INT NOT NULL COMMENT 'ID of the film format',
   `negative_size_id` INT NOT NULL COMMENT 'ID of the negative size',
   PRIMARY KEY (`format_id`, `negative_size_id`),
   INDEX `negative_size_id_idx` (`negative_size_id` ASC),
   CONSTRAINT `format_id`
     FOREIGN KEY (`format_id`)
-    REFERENCES `photography`.`FORMAT` (`format_id`)
+    REFERENCES `FORMAT` (`format_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `negative_size_id`
     FOREIGN KEY (`negative_size_id`)
-    REFERENCES `photography`.`NEGATIVE_SIZE` (`negative_size_id`)
+    REFERENCES `NEGATIVE_SIZE` (`negative_size_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 COMMENT = 'Table to record compatibility between film formats and negative sizes';
